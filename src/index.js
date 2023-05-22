d2ae9a70fb987895346bebc3c51ed2dd6410de42
jdolkowski <jdolkowski@soldevelo.com>
2023-05-22 16:35:06 +0200

    CM-26: update initial structure

--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ const DEFAULT_CONFIG = {
   "translations": [{ key: "en", messages: messages_en }],
 }
 
-export const TemplateModule = (cfg) => {
+export const SocialProtectionModule = (cfg) => {
   return { ...DEFAULT_CONFIG, ...cfg };
 }
