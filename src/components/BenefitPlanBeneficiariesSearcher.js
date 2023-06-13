b40569ef1c157c4341024b8aa1ab083e70dabfa7
sniedzielski <sniedzielski@soldevelo.com>
2023-06-13 20:52:25 +0200

    CM-71: fixed style in div indendations

--- src/components/BenefitPlanBeneficiariesSearcher.js
+++ src/components/BenefitPlanBeneficiariesSearcher.js
@@ -46,20 +46,20 @@ function BenefitPlanBeneficiariesSearcher({
   const applyNumberCircle = (number) => {
     return (
       <div style={{
-          color: '#ffffff',
-          backgroundColor: '#006273',
-          borderRadius: '50%',
-          padding: '5px',
-          minWidth: '40px',
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontWeight: 'bold',
-          fontSize: '12px',
-          width: '20px',
-          height: '45px',
-          marginTop: '7px',
-        }}
+        color: '#ffffff',
+        backgroundColor: '#006273',
+        borderRadius: '50%',
+        padding: '5px',
+        minWidth: '40px',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontWeight: 'bold',
+        fontSize: '12px',
+        width: '20px',
+        height: '45px',
+        marginTop: '7px',
+      }}
       >{number}</div>
     );
   };
