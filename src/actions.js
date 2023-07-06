e52d1c76e67361bebd1de614b0800e3046c592d6
Jan <jdolkowski@soldevelo.com>
2023-07-06 16:12:00 +0200

    CM-139: add core tasks fe (#33)
    * CM-139: add core tasks fe
    
    * CM-139: remove console.log
    
    * CM-139: add loader to table

--- src/actions.js
+++ src/actions.js
@@ -52,6 +52,19 @@ const GROUP_BENEFICIARY_FULL_PROJECTION = () => [
   'status',
 ];
 
+const TASK_FULL_PROJECTION = () => [
+  'id',
+  'entityId',
+  'source',
+  'status',
+  'executorActionEvent',
+  'businessEvent',
+  'dateCreated',
+  'isDeleted',
+  'taskGroup{id, code, completionPolicy}',
+  'data',
+];
+
 const WORKFLOWS_FULL_PROJECTION = () => [
   'name',
   'group',
@@ -70,6 +83,11 @@ export function fetchBeneficiaries(params) {
 export function fetchGroupBeneficiaries(params) {
   const payload = formatPageQueryWithCount('groupBeneficiary', params, GROUP_BENEFICIARY_FULL_PROJECTION());
   return graphql(payload, ACTION_TYPE.SEARCH_GROUP_BENEFICIARIES);
+}
+
+export function fetchBenefitPlanTasks(params) {
+  const payload = formatPageQueryWithCount('task', params, TASK_FULL_PROJECTION());
+  return graphql(payload, ACTION_TYPE.SEARCH_BENEFIT_PLAN_TASKS);
 }
 
 export function fetchBeneficiariesGroup(modulesManager, variables) {
