ad1045f17010901e7db843846893972688762bcf
Dragos DOBRE <dragos.dobre@swisstph.ch>
2023-10-18 15:49:57 +0200

    MERGING RELEASE branches (#52)
    * CM-335: fetch alphabetically ordered benefit plans (#51)
    
    Co-authored-by: Jan <j.dolkowski@soldevelo.com>
    
    * CM-334: change how benefit plan is filtered in picker (#50)
    
    * CM-334: change bf name filtering
    
    * CM-334: add placeholder translation
    
    ---------
    
    Co-authored-by: Jan <j.dolkowski@soldevelo.com>
    
    * add-sonar-ci: add sonar files (#53)
    
    Co-authored-by: Jan <j.dolkowski@soldevelo.com>
    
    ---------
    
    Co-authored-by: Jan <jdolkowski@soldevelo.com>
    Co-authored-by: Jan <j.dolkowski@soldevelo.com>

--- src/pickers/BenefitPlanPicker.js
+++ src/pickers/BenefitPlanPicker.js
@@ -32,7 +32,7 @@ function BenefitPlanPicker(props) {
     query BenefitPlanPicker(
     $search: String, $first: Int, $isDeleted: Boolean
     ) {
-      benefitPlan(name_Icontains: $search, code_Icontains: $search, first: $first, isDeleted: $isDeleted) {
+      benefitPlan(search: $search, first: $first, isDeleted: $isDeleted, sortAlphabetically: true) {
         edges {
           node {
             id
