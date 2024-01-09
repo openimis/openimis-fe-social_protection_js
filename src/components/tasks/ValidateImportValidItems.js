06a7f5592596a67af57d303b5f94f8004af9cf48
sniedzielski <sniedzielski@soldevelo.com>
2024-01-09 18:56:56 +0100

    CM-420: removed object entity comparison

--- src/components/tasks/ValidateImportValidItems.js
+++ src/components/tasks/ValidateImportValidItems.js
@@ -1,26 +1,5 @@
-import React from 'react';
-import { FormattedMessage } from '@openimis/fe-core';
+const ValidateImportValidItemsTaskTableHeaders = () => [];
 
-const ValidateImportValidItemsTaskTableHeaders = () => [
-  <FormattedMessage module="socialProtection" id="benefitPlan.code" />,
-  <FormattedMessage module="socialProtection" id="benefitPlan.name" />,
-  <FormattedMessage module="socialProtection" id="benefitPlan.type" />,
-  <FormattedMessage module="socialProtection" id="benefitPlan.dateValidFrom" />,
-  <FormattedMessage module="socialProtection" id="benefitPlan.dateValidTo" />,
-  <FormattedMessage module="socialProtection" id="benefitPlan.maxBeneficiaries" />,
-  <FormattedMessage module="socialProtection" id="benefitPlan.institution" />,
-  <FormattedMessage module="socialProtection" id="benefitPlan.schema" />,
-];
-
-const ValidateImportValidItemsItemFormatters = () => [
-  (benefitPlan) => benefitPlan?.code,
-  (benefitPlan) => benefitPlan?.name,
-  (benefitPlan) => benefitPlan?.type,
-  (benefitPlan) => benefitPlan?.date_valid_from,
-  (benefitPlan) => benefitPlan?.date_valid_to,
-  (benefitPlan) => benefitPlan?.max_beneficiaries,
-  (benefitPlan) => benefitPlan?.institution,
-  (benefitPlan) => benefitPlan?.beneficiary_data_schema,
-];
+const ValidateImportValidItemsItemFormatters = () => [];
 
 export { ValidateImportValidItemsTaskTableHeaders, ValidateImportValidItemsItemFormatters };
