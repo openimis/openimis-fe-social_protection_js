654504a7eceda1e5179eedc296b08e5ba30a6877
sniedzielski <sniedzielski@soldevelo.com>
2024-01-19 13:58:29 +0100

    CM-420: added fix on frontend to display percentage of invalid items

--- src/components/tasks/ValidateImportValidItems.js
+++ src/components/tasks/ValidateImportValidItems.js
@@ -1,5 +1,18 @@
-const ValidateImportValidItemsTaskTableHeaders = () => [];
+import React from 'react';
+import { FormattedMessage } from '@openimis/fe-core';
 
-const ValidateImportValidItemsItemFormatters = () => [];
+const ValidateImportValidItemsTaskTableHeaders = () => [
+  <FormattedMessage module="socialProtection" id="benefitUploadRecord.benefitPlanCode" />,
+  <FormattedMessage module="socialProtection" id="benefitUploadRecord.sourceName" />,
+  <FormattedMessage module="socialProtection" id="benefitUploadRecord.workflow" />,
+  <FormattedMessage module="socialProtection" id="benefitUploadRecord.percentageOfInvalidItems" />,
+];
+
+const ValidateImportValidItemsItemFormatters = () => [
+  (jsonExt) => jsonExt?.benefit_plan_code,
+  (jsonExt) => jsonExt?.source_name,
+  (jsonExt) => jsonExt?.workflow,
+  (jsonExt) => jsonExt?.percentage_of_invalid_items ?? '-',
+];
 
 export { ValidateImportValidItemsTaskTableHeaders, ValidateImportValidItemsItemFormatters };
