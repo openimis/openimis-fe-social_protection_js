539f63178e3855488664852400efb9b03a84bba2
sniedzielski <sniedzielski@soldevelo.com>
2024-05-23 17:11:56 +0200

    CM-812: updated benefit plan picker, added code of group for benefit group

--- src/pickers/BenefitPlanPicker.js
+++ src/pickers/BenefitPlanPicker.js
@@ -20,6 +20,7 @@ function BenefitPlanPicker(props) {
     onChange,
     filter,
     filterSelectedOptions,
+    type = 'INDIVIDUAL',
   } = props;
 
   const modulesManager = useModulesManager();
@@ -38,6 +39,7 @@ function BenefitPlanPicker(props) {
             id
             code
             name
+            type
             jsonExt
           }
         }
@@ -48,7 +50,9 @@ function BenefitPlanPicker(props) {
     { skip: true },
   );
 
-  const benefitPlans = data?.benefitPlan?.edges.map((edge) => edge.node) ?? [];
+  const benefitPlans = data?.benefitPlan?.edges
+    .map((edge) => edge.node)
+    .filter((node) => node.type === type) ?? [];
   const shouldShowTooltip = benefitPlans?.length >= BENEFIT_PLANS_QUANTITY_LIMIT && !value && !currentString;
 
   return (
