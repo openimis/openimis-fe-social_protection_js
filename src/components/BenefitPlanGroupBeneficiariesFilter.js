57179f24a37a668bd8ad35acaa75f1e8acbb12fa
sniedzielski <52816247+sniedzielski@users.noreply.github.com>
2024-05-24 16:50:50 +0200

    CM-819: fixed searcher of code (#93)

--- src/components/BenefitPlanGroupBeneficiariesFilter.js
+++ src/components/BenefitPlanGroupBeneficiariesFilter.js
@@ -3,13 +3,29 @@ import { injectIntl } from 'react-intl';
 import { Grid } from '@material-ui/core';
 import { withTheme, withStyles } from '@material-ui/core/styles';
 import { formatMessage, TextInput } from '@openimis/fe-core';
+import _debounce from 'lodash/debounce';
 import { defaultFilterStyles } from '../util/styles';
 import BeneficiaryStatusPicker from '../pickers/BeneficiaryStatusPicker';
+import { DEFAULT_DEBOUNCE_TIME, EMPTY_STRING } from '../constants';
 
 function BenefitPlanBeneficiariesFilter({
   intl, classes, filters, onChangeFilters, readOnly,
 }) {
+  const debouncedOnChangeFilters = _debounce(onChangeFilters, DEFAULT_DEBOUNCE_TIME);
+
+  const filterTextFieldValue = (filterName) => filters?.[filterName]?.value ?? EMPTY_STRING;
+
   const filterValue = (filterName) => filters?.[filterName]?.value;
+
+  const onChangeStringFilter = (filterName) => (value) => {
+    debouncedOnChangeFilters([
+      {
+        id: filterName,
+        value,
+        filter: `${filterName}: "${value}"`,
+      },
+    ]);
+  };
 
   return (
     <Grid container className={classes.form}>
@@ -17,14 +33,8 @@ function BenefitPlanBeneficiariesFilter({
         <TextInput
           module="socialProtection"
           label="group.code"
-          value={filterValue('code')}
-          onChange={(value) => onChangeFilters([
-            {
-              id: 'code',
-              value,
-              filter: `group_Code_Icontains: "${value}"`,
-            },
-          ])}
+          value={filterTextFieldValue('group_Code_Icontains')}
+          onChange={onChangeStringFilter('group_Code_Icontains')}
         />
       </Grid>
       <Grid item xs={2} className={classes.item}>
